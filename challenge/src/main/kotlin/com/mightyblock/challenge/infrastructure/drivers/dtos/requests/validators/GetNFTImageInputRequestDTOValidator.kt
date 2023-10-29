8a72ddf8fe31f9063ce5d34d3b8c51b659dd46b5
Cristian <castagna.cristian16@gmail.com>
2023-10-29 20:53:07 -0300

    Add image validations

--- challenge/src/main/kotlin/com/mightyblock/challenge/infrastructure/drivers/dtos/requests/validators/GetNFTImageInputRequestDTOValidator.kt
+++ challenge/src/main/kotlin/com/mightyblock/challenge/infrastructure/drivers/dtos/requests/validators/GetNFTImageInputRequestDTOValidator.kt
@@ -4,9 +4,28 @@ import com.mightyblock.challenge.infrastructure.drivers.dtos.requests.GetNFTImag
 
 class GetNFTImageInputRequestDTOValidator : IInputRequestValidator<GetNFTImageRequestDTO> {
 
+    companion object {
+        val ALLOWED_EXTENSIONS = listOf(
+            "jpg",
+            "jpeg",
+            "png"
+        )
+    }
+
     override fun validate(request: GetNFTImageRequestDTO) {
         if (request.imageName.isBlank()) {
             throw IllegalArgumentException("Image name is required.")
         }
+
+        val extension = getImageExtension(request.imageName)
+
+        if (!hasImageExtension(extension)) {
+            throw IllegalArgumentException("Image extension $extension is not allowed")
+        }
     }
+
+    private fun hasImageExtension(extension: String?) = ALLOWED_EXTENSIONS.contains(extension)
+
+    private fun getImageExtension(imageName: String) =
+        imageName.substringAfterLast('.', "")
 }
